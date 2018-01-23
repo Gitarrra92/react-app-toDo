1e43380987c8c1ef30439443fc2f9f239edc9c89
Natalia Pilewska <pilewska.natalia@gmail.com>
2018-01-23 21:26:56 +0100

    Task functions added

--- src/ToDo.js
+++ src/ToDo.js
@@ -5,13 +5,26 @@ import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 import {List, ListItem} from 'material-ui/List';
 import ActionDelete from 'material-ui/svg-icons/action/delete';
-
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 
 
 const Task = (props) => (
     <ListItem
+        style={
+            props.taskDone === false ?
+                {textDecoration: 'none'}
+                :
+                {textDecoration: 'line-through', color: '#999'}
+        }
         primaryText={props.taskName}
-        rightIcon={<ActionDelete onClick={()=>props.deleteTask(props.taskId)}/>}
+        rightIcon={<ActionDelete onClick={() => props.deleteTask(props.taskId)}/>}
+        leftIcon={
+            props.taskDone === false ?
+                <ActionFavoriteBorder onClick={() => props.toggleDoneTask(props.taskId, props.taskDone)}/>
+                :
+                <ActionFavorite onClick={() => props.toggleDoneTask(props.taskId, props.taskDone)}/>
+        }
     />
 )
 
@@ -25,56 +38,71 @@ class ToDo extends React.Component {
         database.ref('/ToDo')
             .on('value', (snapshot) => {
                     const arrTasks = Object.entries(
-                        snapshot.val() || {}).map(([key,value])=>{value.key = key; return value})
+                        snapshot.val() || {}).map(([key, value]) => {
+                        value.key = key;
+                        return value
+                    })
                     this.setState({tasks: arrTasks})
                 }
             )
     }
 
     deleteTask = (taskId) => {
-        database.ref('/ToDo/'+taskId)
+        database.ref('/ToDo/' + taskId)
             .remove()
     }
 
+    toggleDoneTask = (taskId, taskDone) => {
+        database.ref('/ToDo/' + taskId)
+            .update({done: !taskDone})
+            .then(() => console.log('toggleDoneTask resolved OK'))
+    }
 
 
     handleAddTask = () => {
-        if (!this.state.textFromInput){
+        if (!this.state.textFromInput) {
             alert('empty input');
             return
         }
 
         database.ref('/ToDo')
-            .push({name: this.state.textFromInput})
-        this.setState({textFromInput : ''})
+            .push(
+                {
+                    name: this.state.textFromInput,
+                    done: false
+                }
+            )
+        this.setState({textFromInput: ''})
     }
 
     render() {
         return (
             <div>
                 <TextField
-                    hintText={"Add your text..."}
+                    hintText={"Write your text..."}
                     fullWidth={true}
                     value={this.state.textFromInput}
-                    onChange={(e,value)=> this.setState({textFromInput: value})}
+                    onChange={(e, value) => this.setState({textFromInput: value})} // onChange={this.handleTextFromInput}
                 />
                 <RaisedButton
-                    label={"add"}
+                    label={"add your task"}
                     secondary={true}
                     fullWidth={true}
                     onClick={this.handleAddTask}
                 />
 
-                <List style={{textAlign:'left'}}>
+                <List style={{textAlign: 'left'}}>
                     {
                         this.state.tasks
                         &&
                         this.state.tasks.map((el) => (
                             <Task
-                                taskName = {el.name}
-                                taskId = {el.key}
-                                deleteTask = {this.deleteTask}
-                                key = {el.key}
+                                taskId={el.key}
+                                taskName={el.name}
+                                taskDone={el.done}
+                                deleteTask={this.deleteTask}
+                                toggleDoneTask={this.toggleDoneTask}
+                                key={el.key}
                             />
                         ))
                     }
